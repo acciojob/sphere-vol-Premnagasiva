d6ffe7b56bd37413ac9d963857313dec245a1d73
Prem Naga Siva <premnagasiva@gmail.com>
2026-01-06 22:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+  const radius = Number(e.target.radius.value)
+	const Volume = e.target.volume;
+	if(isNaN(radius) || radius<=0){
+		Volume.value = 'NaN'
+		return;
+	}
+	Volume.value = (4/3 * Math.PI*(radius * radius * radius)).toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
